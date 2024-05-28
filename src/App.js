2686676ec8cf63a9a644e2144810c1892533dfbd
Caliber 49 <2528357407@qq.com>
2024-05-28 15:41:18 +0800

    5.28Submission

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import ToDoList from './components/ToDoList'
+import './App.css'
+
+export default function App () 
+{
+  return (
+    <div className='App'>
+      <ToDoList />
+    </div>
+  )
+}
